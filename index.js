3460f3ff3f4a90ac4b9086514c068c072efd6953
Asad Mahmood <asadmhmd1234@gmail.com>
2022-07-03 15:48:50 +0530

    Add express to the app and convert from plain nodejs to express app

--- index.js
+++ index.js
@@ -1,31 +1,46 @@
-const http = require("http");
+const express = require("express");
+// const http = require("http");
 const path = require("path");
-const fs = require("fs");
+// const fs = require("fs");
 
-const server = http.createServer((req, res) => {
-  const fileName = (req.url === "/" ? "/index" : req.url) + ".html";
-  fs.readFile(path.join(__dirname, fileName), "utf-8", (err, content) => {
-    if (err) {
-      if (err.code === "ENOENT") {
-        fs.readFile(
-          path.join(__dirname, "404.html"),
-          "utf-8",
-          (err, content) => {
-            if (err) throw err;
-            res.writeHead(404, { "Content-Type": "text/html" });
-            res.end(content);
-          }
-        );
-      } else throw err;
-    } else {
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.end(content);
-    }
-  });
-});
+const app = express();
 
 const PORT = process.env.PORT || 8080;
+// const server = http.createServer((req, res) => {
+//   const fileName = (req.url === "/" ? "/index" : req.url) + ".html";
+//   fs.readFile(path.join(__dirname, fileName), "utf-8", (err, content) => {
+//     if (err) {
+//       if (err.code === "ENOENT") {
+//         fs.readFile(
+//           path.join(__dirname, "404.html"),
+//           "utf-8",
+//           (err, content) => {
+//             if (err) throw err;
+//             res.writeHead(404, { "Content-Type": "text/html" });
+//             res.end(content);
+//           }
+//         );
+//       } else throw err;
+//     } else {
+//       res.writeHead(200, { "Content-Type": "text/html" });
+//       res.end(content);
+//     }
+//   });
+// });
 
-server.listen(PORT, () => {
-  console.log("The application is listening on port:", PORT);
+app.get("/about", (req, res, next) => {
+  res.sendFile(path.join(__dirname, "about.html"));
+});
+app.get("/contact-me", (req, res, next) => {
+  res.sendFile(path.join(__dirname, "contact-me.html"));
+});
+app.get("/", (req, res, next) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, "404.html"));
+});
+app.listen(PORT, () => {
+  console.log("The app is listening at PORT:", PORT);
 });
